b14efd32d2b74fe3a819487083e3811731ddd4a8
Tobiasre2003 <tobiasre@student.chalmers.se>
2025-02-26 10:48:02 +0100

    ny test

--- src/go_to_goal/test.py
+++ src/go_to_goal/test.py
@@ -3,22 +3,34 @@
 import rospy
 from gv_client.msg import GulliViewPosition
 
-class Test:
+class Point:
+    def __init__(self, id, x, y, theta):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.theta = theta
+        
+    def __str__(self):
+        return f'(Id: {self.id}, X: {self.x}, Y: {self.y}, Theta: {self.theta})'
+
+
+class Receiver:
     def __init__(self):
         rospy.init_node('test', anonymous=True)
         rospy.loginfo("Starting test node")
-        rospy.Subscriber('gv_positions', GulliViewPosition, self._position_cb)
-
-    def _position_cb(self, position_msg):
-        self.x = position_msg.x
-        self.y = position_msg.y
-        self.theta = position_msg.theta
-        self.id = position_msg.tagId
-        print(self.x,self.y,self.theta, self.id)
+        rospy.Subscriber('gv_positions', GulliViewPosition, self.positions)
+    
+    def positions(self, position_msg):
+        id = position_msg.tagId
+        x = position_msg.x
+        y = position_msg.y
+        theta = position_msg.theta
+        self.p = Point(id, x, y, theta)
+        print(self.p)
 
 
 if __name__ == '__main__': 
-    node = Test()
+    node = Receiver()
     rospy.spin()
     
     
